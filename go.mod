625cfeb3af05fcdcd6b341aa70e5ee162bcbf280
theocheng <zlz_gty@foxmail.com>
2023-09-15 20:27:13 +0800

    init

---
+++ go.mod
@@ -0,0 +1,23 @@
+module cj
+
+go 1.18
+
+require xorm.io/xorm v1.3.3
+
+require (
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+)
+
+require (
+	github.com/goccy/go-json v0.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gookit/color v1.5.4
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	modernc.org/sqlite v1.25.0 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
+)
---
+++ main.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	// _ "database/sql"
+	"embed"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3" // need cgo, bud support windows
+	// _ "modernc.org/sqlite" // cgo free, but don't support windows
+	"xorm.io/xorm"
+
+	// color
+	"github.com/gookit/color"
+)
+
+type Entry struct {
+	Char  string
+	Forms []string
+}
+
+//go:embed cangjie.db
+var dbFile embed.FS
+
+var (
+	green = color.FgGreen.Render
+	blue  = color.FgBlue.Render
+	red   = color.FgRed.Render
+
+	bgLightYellow = color.BgLightYellow.Render
+)
+
+const HelpInfo = `Usage:
+  cj [query]		查询
+  cj -r | --reinstall	重建数据库`
+
+func init() {
+
+	// if not exist cangjie.db
+	// then create it
+	if _, err := os.Stat(dbPath("cangjie.db")); os.IsNotExist(err) {
+		err = installDB()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
+
+	var engine, err = xorm.NewEngine("sqlite3", dbPath("cangjie.db"))
+	if err != nil {
+		panic(err)
+	}
+	querys := os.Args[1:]
+
+	if len(querys) == 0 {
+		fmt.Println(HelpInfo)
+		os.Exit(1)
+	}
+
+	if querys[0] == "-r" || querys[0] == "--reinstall" {
+		err = reinstallDB()
+		if err != nil {
+			panic(err)
+		}
+		color.Redln("cangjie.db 重建成功")
+		os.Exit(0)
+	}
+
+	for _, query := range querys {
+		entry := new(Entry)
+		_, err = engine.Table("entry").Where("char=?", query).Get(entry)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if len(entry.Forms) > 1 {
+			if entry.Forms[0] == entry.Forms[1] {
+				entry.Forms = entry.Forms[:1]
+			}
+		}
+
+		for _, form := range entry.Forms {
+			fmt.Printf(bgLightYellow(red(" > %s < ")), entry.Char)
+			fmt.Print(green(" "))
+			for _, char := range form {
+				fmt.Print(green(atohan(string(char))))
+			}
+			fmt.Print(" ")
+			for _, char := range form {
+				fmt.Printf("%s", blue(string(char)))
+			}
+
+			fmt.Println()
+		}
+		// fmt.Printf("%T\n", entry.Forms)
+	}
+}
+
+// Ascii to Hans
+func atohan(s string) string {
+	table := map[string]string{
+		"a": "日",
+		"b": "月",
+		"c": "金",
+		"d": "木",
+		"e": "水",
+		"f": "火",
+		"g": "土",
+		"h": "竹",
+		"i": "戈",
+		"j": "十",
+		"k": "大",
+		"l": "中",
+		"m": "一",
+		"n": "弓",
+		"o": "人",
+		"p": "心",
+		"q": "手",
+		"r": "口",
+		"s": "尸",
+		"t": "廿",
+		"u": "山",
+		"v": "女",
+		"w": "田",
+		"x": "難",
+		"y": "卜",
+		"z": "符",
+	}
+	return table[s]
+}
+
+// reinstallDB is a function that reinstall the database.
+// It removes the database if it already exists,
+// and then installs it from the embedded file.
+//
+// It returns an error if there is any error during the process.
+func reinstallDB() error {
+	if _, err := os.Stat(dbPath("cangjie.db")); os.IsNotExist(err) {
+		err = os.Remove(dbPath("cangjie.db"))
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := installDB(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// installDB installs the database.
+//
+// It reads the contents of the "cangjie.db" file using the dbFile.ReadFile function
+// and creates a new "cangjie.db" file using the os.Create function. It then writes the
+// data read from the file to the new file using the cangjie.Write function. Finally,
+// it calls the cangjie.Sync function to ensure that the data is written to disk,
+// and closes the file using the defer statement. It returns an error if any of the
+// file operations fail.
+//
+// The installDB function has no parameters and returns an error.
+func installDB() error {
+
+	data, err := dbFile.ReadFile("cangjie.db")
+	if err != nil {
+		return err
+	}
+
+	cangjie, err := os.Create(dbPath("cangjie.db"))
+	if err != nil {
+		return err
+	}
+
+	_, err = cangjie.Write(data)
+	if err != nil {
+		return err
+	}
+
+	err = cangjie.Sync()
+	if err != nil {
+		return err
+	}
+	defer func(cangjie *os.File) {
+		err := cangjie.Close()
+		if err != nil {
+			panic("Fail to close file....")
+		}
+	}(cangjie)
+
+	return nil
+}
+
+// dbPath returns the path to the SQLite database file.
+//
+// It takes a dbName string parameter, which specifies the name of the database.
+// It returns a string, which is the path to the SQLite database file.
+// It will choose the User Cache Dir to store the database file.
+func dbPath(dbName string) string {
+	// exe, err := os.Executable()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// exePath := filepath.Dir(exe)
+
+	// sqlitePath := filepath.Join(exePath, dbName)
+	// return sqlitePath
+
+	// use User Cache Dir to store cangjie.db
+
+	// if not exist %UserCacheDir%\cangjie folder
+	// then create it
+
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = os.Stat(filepath.Join(cacheDir, "cangjie"))
+	if os.IsNotExist(err) {
+		err = os.Mkdir(filepath.Join(cacheDir, "cangjie"), os.ModePerm)
+		if err != nil {
+			panic("Fail to create cache dir")
+		}
+	}
+
+	return filepath.Join(cacheDir, "cangjie", dbName)
+}
