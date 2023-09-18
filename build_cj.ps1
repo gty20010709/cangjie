8f9951b2787670045da11a13d218c513f58441ed
theocheng <zlz_gty@foxmail.com>
2023-09-18 10:17:31 +0800

    build: make the build script more portable

--- build_cj.ps1
+++ build_cj.ps1
@@ -1,18 +1,21 @@
+$AppName = "cj"
+
+
 Write-Output "Start Building..."
 
 Write-Output "Building for Linux amd64..."
-$ENV:CGO_ENABLED = 0; $ENV:GOOS = "linux"; $ENV:GOARCH = "amd64"; go build --ldflags "-s -w" -o dist/cj_linux_amd64/cj
-tar -zcvf dist/cj_linux_amd64.tar.gz dist/cj_linux_amd64
+$ENV:CGO_ENABLED = 0; $ENV:GOOS = "linux"; $ENV:GOARCH = "amd64"; go build --ldflags "-s -w" -o dist/$($AppName)_linux_amd64/$($AppName)
+tar -zcvf dist/$($AppName)_linux_amd64.tar.gz dist/$($AppName)_linux_amd64
 Write-Output "Building for darwin amd64..."
-$ENV:CGO_ENABLED = 0; $ENV:GOOS = "darwin"; $ENV:GOARCH = "amd64"; go build --ldflags "-s -w" -o dist/cj_darwin_amd64/cj
-tar -zcvf dist/cj_darwin_amd64.tar.gz dist/cj_darwin_amd64
+$ENV:CGO_ENABLED = 0; $ENV:GOOS = "darwin"; $ENV:GOARCH = "amd64"; go build --ldflags "-s -w" -o dist/$($AppName)_darwin_amd64/$($AppName)
+tar -zcvf dist/$($AppName)_darwin_amd64.tar.gz dist/$($AppName)_darwin_amd64
 Write-Output "Building for windows amd64..."
-$ENV:CGO_ENABLED = 0; $ENV:GOOS = "windows"; $ENV:GOARCH = "amd64"; go build --ldflags "-s -w" -o dist/cj_windows_amd64/cj.exe
+$ENV:CGO_ENABLED = 0; $ENV:GOOS = "windows"; $ENV:GOARCH = "amd64"; go build --ldflags "-s -w" -o dist/$($AppName)_windows_amd64/$($AppName).exe
 
 $compress = @{
-    Path             = "dist/cj_windows_amd64"
+    Path             = "dist/$($AppName)_windows_amd64"
     CompressionLevel = "Fastest"
-    DestinationPath  = "dist/cj_windows_amd64.zip"
+    DestinationPath  = "dist/$($AppName)_windows_amd64.zip"
     Force            = $true
 }
 Compress-Archive @compress
@@ -20,9 +23,9 @@ Compress-Archive @compress
 
 Write-Output "Build and Compress Complete. Cleaning up..."
 # force remove
-Remove-Item "dist/cj_linux_amd64" -Force -Recurse
-Write-Output "rm dist/cj_linux_amd64" 
-Remove-Item dist/cj_darwin_amd64 -Force -Recurse
-Write-Output "rm dist/cj_darwin_amd64"
-Remove-Item dist/cj_windows_amd64 -Force -Recurse
-Write-Output "rm dist/cj_windows_amd64"
+Remove-Item "dist/$($AppName)_linux_amd64" -Force -Recurse
+Write-Output "rm dist/$($AppName)_linux_amd64" 
+Remove-Item dist/$($AppName)_darwin_amd64 -Force -Recurse
+Write-Output "rm dist/$($AppName)_darwin_amd64"
+Remove-Item dist/$($AppName)_windows_amd64 -Force -Recurse
+Write-Output "rm dist/$($AppName)_windows_amd64"
